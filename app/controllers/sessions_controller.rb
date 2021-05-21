12118346d31e26fd301284a3e26fc4469336d7a8
Eli Cusic <tntitans31@gmail.com>
2021-05-21 10:38:53 -0500

    filled out users controller and created sessions controller and view

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,48 @@
+class SessionsController < ApplicationController
+    # skip_before_action :require_login
+    # skip_before_action :validate_user
+
+    def new
+    end
+
+    # def create
+    #     @user = User.find_by(email: session_params[:email])
+    #     if @user && @user.authenticate(session_params[:password])
+    #         initialize_session
+    #         redirect_to current_user.url
+    #     else
+    #         flash[:error] = "Invalid email or password. Please try again."
+    #         redirect_to '/login'
+    #     end
+    # end
+
+    # def create_with_omniauth
+    #     @user = User.find_by(email: auth['info']['email'])
+    #     #does this account for invalid data sent from facebook or wherever?
+    #     if !@user
+    #         User.new_user_omni
+    #     end
+    #     initialize_session
+    #     redirect_to @user.url
+    # end
+
+    # def destroy
+    #     session.delete(:user_id)
+    #     redirect_to '/'
+    # end
+
+    private
+
+    def session_params
+        params.require(:user).permit(:email, :password)
+    end
+
+    def initialize_session
+        session[:user_id] = @user.id
+    end
+
+    # def auth
+    #     request.env['omniauth.auth']
+    # end
+    
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,40 @@
 class UsersController < ApplicationController
+    before_action :current_user, only: [:show, :edit, :update, :destroy]
+    # skip_before_action :require_login, only: [:new, :create]
+    # skip_before_action :validate_user, only: [:new, :create, :show]
+
+    def new
+        @user = new_user
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save   
+            session[:user_id] = @user.id
+            redirect_to @user.url
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @user.update(user_params)
+            redirect_to @user.url
+        else 
+            render :edit
+        end
+    end
+
+    def destroy
+        current_user.destroy
+        session.delete(:user_id)
+        redirect_to '/'
+    end
+
 end
