449a56d971d1e94b9893ea5d3519a062fcb3f648
Eli Cusic <tntitans31@gmail.com>
2021-05-26 10:30:57 -0500

    set up create method for deck controller

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -10,13 +10,12 @@ class DecksController < ApplicationController
     end
 
     def create  
-        deck_params
-        # @deck = Appointment.new_with_params(deck_params)
-        # if @deck.save
-        #     redirect_to user_path(current_user)
-        # else
-        #     render :new
-        # end
+        @deck = Deck.new(deck_params)
+        if @deck.save
+            redirect_to_profile
+        else
+            render :new
+        end
     end
 
     def show
@@ -27,15 +26,15 @@ class DecksController < ApplicationController
 
     def update
         if @deck.update(deck_params)
-            redirect_to user_path(current_user)
+            redirect_to_profile
         else
             render :edit
         end
     end
 
     def destroy
-        # @deck.destroy
-        # redirect_to 
+        @deck.destroy
+        redirect_to_profile
     end
 
     private 
@@ -49,6 +48,10 @@ class DecksController < ApplicationController
 
     def deck
         @deck = Deck.find_by_id(params[:id])
+    end
+
+    def redirect_to_profile
+        redirect_to user_path(current_user)
     end
 
 end
