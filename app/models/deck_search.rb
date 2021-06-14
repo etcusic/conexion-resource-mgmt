4eff78b0824b5df5dd50b3cabfbf2fe6821b7f40
Eli Cusic <tntitans31@gmail.com>
2021-06-14 09:47:41 -0500

    streamlined deck search querying method

--- app/models/deck_search.rb
+++ app/models/deck_search.rb
@@ -12,21 +12,11 @@ class DeckSearch
     end
 
     def decks 
-        # 1) filter admin approved, 2) filter level, 3) filter user
-        decks_array = filter_user(filter_level(filter_admin))
-        # decks_array = filter_admin.filter_level.filter_user
+        Deck.where(self.as_hash)
     end
 
-    def filter_admin
-        self.admin_approved == "" ? Deck.all : Deck.where({ admin_approved: self.admin_approved })
-    end
-
-    def filter_level(decks_array)
-        self.level == "" ? decks_array : decks_array.where({ level: self.level })
-    end
-
-    def filter_user(decks_array)
-        self.user_id == "" ? decks_array : decks_array.where({ user_id: self.user_id })
+    def as_hash
+        self.as_json.delete_if{|key, value| value == ""}
     end
 
 end
