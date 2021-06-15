b67fb29f4ad17fb0b110dd165a73cfcbaa5679f6
Eli Cusic <tntitans31@gmail.com>
2021-06-15 10:32:13 -0500

    set up partials to differentiate between admin and reg users

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -4,7 +4,6 @@ class Deck < ApplicationRecord
     validates :name, presence: true, :length => {:maximum => 50} 
     validates :cards, :presence => true, :length => {:minimum => 3, :maximum => 50}
     accepts_nested_attributes_for :cards, allow_destroy: true
-    # admin clones a deck rather than edits existing => send an alert if original deck is edited; give option to update accordingly
 
     def with_cards
         @deck = {
@@ -15,12 +14,6 @@ class Deck < ApplicationRecord
         }
         self.cards.each{ |card| @deck[:cards] << { id: card.id, english: card.english, spanish: card.spanish } }
         @deck
-    end
-
-    def self.scoped_decks(params)
-        @decks = []
-        !params[:admin_approved] || params[:admin_approved] == "" ? @decks = self.all : @decks = self.where("admin_approved = #{params[:admin_approved]}")
-        @decks
     end
     
 end
