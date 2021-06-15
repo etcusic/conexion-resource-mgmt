2a0c758727c56a444501f92649ed6f77867ae686
Eli Cusic <tntitans31@gmail.com>
2021-06-15 16:13:47 -0500

    set up admin decks to receive existing deck and put into form

--- app/controllers/admin_decks_controller.rb
+++ app/controllers/admin_decks_controller.rb
@@ -4,10 +4,11 @@ class AdminDecksController < ApplicationController
     end
 
     def new
-        binding.pry
+        @admin_deck = params[:format] ? new_admin_deck : AdminDeck.new
     end
 
     def create
+        binding.pry
     end
 
     def show
@@ -26,6 +27,11 @@ class AdminDecksController < ApplicationController
 
     def admin_deck_params
 
+    end
+
+    def new_admin_deck
+        original_deck = Deck.find_by_id(params[:format])
+        admin_deck = AdminDeck.new_from_existing(original_deck, current_user.id)
     end
 
 end
